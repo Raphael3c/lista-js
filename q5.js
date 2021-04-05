b52bb155e28540c1a6c385ab7038964df489ef10
Raphael3c <phael.ufc@gmail.com>
2021-04-05 19:14:51 -0300

    Questão 5, utilizando replace

--- q5.js
+++ q5.js
@@ -1,1 +1,12 @@
-console.log("Vou fazer essa questão amanhaã!")
+const dinheiro = (valor) => {
+
+    const valorFixed = valor.toFixed(2);
+    const valorString = String(valorFixed);
+    const valorReais = valorString.replace(',' , '.');
+    const result = `R$${valorReais}`;
+
+    console.log(result);    
+}
+
+
+dinheiro(0.1+0.2)
