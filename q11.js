a411a99a5c93c5e75a850e21dc63b2b7dbd03625
Raphael3c <phael.ufc@gmail.com>
2021-04-12 20:54:41 -0300

    Questão 11

---
+++ q11.js
@@ -0,0 +1,12 @@
+const eBissexto = (ano) => {
+
+    if(ano%4 == 0 && ano%100 != 0) return true;
+
+    else if(ano%400 == 0 ) return true;
+
+    else return false;
+
+}
+
+
+console.log(eBissexto(2020))
