e06d2e61e30f6b035ea1da65e070eaaaa68d20f4
Raphael3c <phael.ufc@gmail.com>
2021-04-12 21:07:58 -0300

    Questão 13

---
+++ q13.js
@@ -0,0 +1,38 @@
+const dia = (dia) => {
+    switch(dia){
+        case 1:
+            console.log("Fim de semana");
+            break;
+
+        case 2:
+            console.log("Dia útil");
+            break;
+        
+        case 3:
+            console.log("Dia útil");
+            break;
+
+        case 4:
+            console.log("Dia útil");
+            break;
+
+        case 5:
+            console.log("Dia útil");
+            break;
+
+        case 6:
+            console.log("Dia útil");
+            break;
+
+
+        case 7:
+            console.log("Fim de semana");
+            break;
+
+        default:
+            console.log("Dia inválido")
+            break;
+    }
+}
+
+dia(1);
